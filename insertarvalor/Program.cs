abbdfa83eb615d9faa578a66f6badab01e4d45da
Soyfernand00 <n00443358@upn.pe>
2024-10-31 10:43:31 -0500

    s12

---
+++ insertarvalor/Program.cs
@@ -0,0 +1,26 @@
+﻿void imprime(int[] lis)
+{
+    foreach (int i in lis)
+    {
+        Console.WriteLine(i);
+    }
+}
+int[] lista = { 5, 6, 8, 9 };
+imprime(lista);
+
+//pedir numero y posicion
+//aumentar tamaño de arreglo
+//desplazar numeros hacia el final hasta insertar nuevo valor
+
+Console.WriteLine("Ingresar nuevo:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresar posicion:");
+int p = int.Parse(Console.ReadLine());
+
+Array.Resize(ref lista, lista.Length+1);
+for (int i = lista.Length-1; i > p; i--)
+{
+    lista[i]=lista[i-1];
+}
+lista[p] = n;
+imprime(lista);
---
+++ semana12/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] lista=new int[5];
+Random r = new Random();
+for (int i = 0; i < lista.Length; i++)
+{
+    lista[i] = r.Next(1,11);
+}
+foreach  (int i in lista)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Ingresar nuevo:");
+int n=int.Parse(Console.ReadLine());
+Array.Resize(ref lista, lista.Length+1);
+lista[lista.Length-1] = n;
+
+foreach (int i in lista)
+{
+    Console.WriteLine(i);
+}
+
+/*Console.WriteLine("Ingresar nuevo:");
+int n=int.Parse(Console.ReadLine());
+Array.Resize(ref lista, n);
+lista[lista.Length-1] = n;*/
+
+foreach (int i in lista)
+{
+    Console.WriteLine(i);
+}
